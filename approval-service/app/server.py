711c1128bcaeb91b37dc3eb2f54126ed429f1997
Piotr Mucha <piotr.mucha@telefonicatech.uk>
2025-01-30 10:07:07 +0000

    Added base url to output on error

--- approval-service/app/server.py
+++ approval-service/app/server.py
@@ -80,5 +80,5 @@ async def process_metadata_request(
     except httpx.RequestError:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Error connecting to the metadata service",
+            detail="Error connecting to the metadata service. Base url: " + url_base,
         ) from None
